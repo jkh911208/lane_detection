53458793107db257c4764d59801c19c1d5fc11d5
James Jeon <jkh911208@naver.com>
2017-05-04 16:30:21 -0500

    on screen direction and draw only two lanes

--- lane.py
+++ lane.py
@@ -1,16 +1,23 @@
 import cv2
 import numpy as np
+from numpy.polynomial import Polynomial as P
 
-cap = cv2.VideoCapture('sample/sample_21.mp4')
+
+cap = cv2.VideoCapture('sample/sample_0.mp4')
+cap.set(cv2.CAP_PROP_FPS, 15)
 width = 1150
 height = 720
 guide_left_s = (500 - 600) / ((width/2 - 70) - (width/2 - 200))
 guide_right_s = (500 - 600) / ((width/2 + 70) - (width/2 + 200))
+PREV_LEFT_X1 = None
+PREV_LEFT_X2 = None
+PREV_RIGHT_X1 = None
+PREV_RIGHT_X2 = None
 
 def filter_color(image):
 	# Blur the Image
 	image_blur = image
-	#image_blur = cv2.GaussianBlur(image,(3,3),0)
+	image_blur = cv2.GaussianBlur(image,(3,3),0)
 	#return image_blur
 
 	gray_min = np.array([100, 100, 100], dtype=np.uint8)
@@ -50,8 +57,6 @@ def find_lines(image):
 	g1 = np.array([[width/2 - 70,500,width/2 - 200,600]], dtype=np.int32)
 	g2 = np.array([[width/2 + 70,500,width/2 + 200,600]], dtype=np.int32)
 	g3 = np.array([[width/2,0,width/2,720]], dtype=np.int32)
-	#print((float(g1[0][3]) - g1[0][1]) / (float(g1[0][2]) - g1[0][0]))
-	#print((float(g2[0][3]) - g2[0][1]) / (float(g2[0][2]) - g2[0][0]))
 
 	guide_lines= [g1,g2,g3]
 	
@@ -64,8 +69,13 @@ def slope(line):
 		return (float(line[3]) - line[1]) / (float(line[2]) - line[0])
 
 def draw_lanes(image, lines):
-	last_left_s = np.array([])
-	last_right_s = np.array([])
+	current_left_s = np.array([])
+	current_right_s = np.array([])
+	global PREV_LEFT_X1, PREV_LEFT_X2, PREV_RIGHT_X1, PREV_RIGHT_X2
+	left_x = np.array([])
+	left_y = []
+	right_x = []
+	right_y = []
 	try:
 		for line in lines:
 			coords = line[0]
@@ -79,30 +89,62 @@ def draw_lanes(image, lines):
 				continue
 
 			if (guide_left_s * 1.4) < s < (guide_left_s * 0.6):
-				cv2.line(image,(coords[0],coords[1]),(coords[2], coords[3]), [255,0,0],3)
-				if coords[0] < (width / 2) or coords[2] < (width / 2):
-					last_left_s = np.append(last_left_s, s)
-			elif 0.5 < s < 1:
-				cv2.line(image,(coords[0],coords[1]),(coords[2], coords[3]), [0,255,0],3)
-				if coords[0] > (width / 2) or coords[2] > (width / 2):
-					last_right_s = np.append(last_right_s, s)
-			elif s == 0:
-				cv2.line(image,(coords[0],coords[1]),(coords[2], coords[3]), [0,0,255],3)
+				if coords[0] < (width / 2) and coords[2] < (width / 2):
+					current_left_s = np.append(current_left_s, s)
+					left_x = np.append(left_x, (coords[0], coords[2]), axis=0) 
+					left_y += [coords[1], coords[3]]
+			elif (guide_right_s * 0.6) < s < (guide_right_s * 1.4):
+				if coords[0] > (width / 2) and coords[2] > (width / 2):
+					current_right_s = np.append(current_right_s, s)
+					right_x += [coords[0], coords[2]]
+					right_y += [coords[1], coords[3]]
 	except:
 		pass
 
-	mean_left_s = np.mean(last_left_s)
-	mean_right_s = np.mean(last_right_s)
+	y1 = height
+	y2 = height / 2 + 120
+	
+	if len(left_x) <= 1 or len(right_x) <= 1:
+		#if PREV_LEFT_X1 is not None:
+			#cv2.line(image, (int(PREV_LEFT_X1), int(y1)), (int(PREV_LEFT_X2), int(y2)), [255,0,0],3)
+			#cv2.line(image, (int(PREV_RIGHT_X1), int(y1)), (int(PREV_RIGHT_X2), int(y2)), [0,255,0],3)
+		return
 
-	if mean_left_s > guide_left_s and mean_right_s > guide_right_s:
-		print("Turning Right")
-	elif mean_left_s < guide_left_s and mean_right_s < guide_right_s:
-		print("Turning Left")
-	elif mean_left_s > guide_left_s and mean_right_s < guide_right_s:
-		print("Straight")
+	left_poly = P.fit(np.array(left_x), np.array(left_y), 1)
+	right_poly = P.fit(np.array(right_x), np.array(right_y), 1)
 
+	left_x1 = (left_poly - y1).roots()
+	right_x1 = (right_poly - y1).roots()
 
+	left_x2 = (left_poly - y2).roots()
+	right_x2 = (right_poly - y2).roots()
 
+	if PREV_LEFT_X1 is not None:
+		left_x1 = PREV_LEFT_X1 * 0.7 + left_x1 * 0.3
+		left_x2 = PREV_LEFT_X2 * 0.7 + left_x2 * 0.3
+		right_x1 = PREV_RIGHT_X1 * 0.7 + right_x1 * 0.3
+		right_x2 = PREV_RIGHT_X2 * 0.7 + right_x2 * 0.3
+
+	PREV_LEFT_X1 = left_x1
+	PREV_LEFT_X2 = left_x2
+	PREV_RIGHT_X1 = right_x1
+	PREV_RIGHT_X2 = right_x2
+
+	cv2.line(image, (int(left_x1), int(y1)), (int(left_x2), int(y2)), [255,0,0],3)
+	cv2.line(image, (int(right_x1), int(y1)), (int(right_x2), int(y2)), [0,255,0],3)
+
+	mean_left_s = np.mean(current_left_s)
+	mean_right_s = np.mean(current_right_s)
+
+	if mean_left_s > guide_left_s * 1.1 and mean_right_s > guide_right_s * 1.1:
+		#print("Turning Right")
+		cv2.putText(image,'Turning Right',(10,700), cv2.FONT_HERSHEY_COMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
+	elif mean_left_s < guide_left_s * 1.1 and mean_right_s < guide_right_s * 1.1:
+		#print("Turning Left")
+		cv2.putText(image,'Turning Left',(10,700), cv2.FONT_HERSHEY_COMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
+	elif mean_left_s > guide_left_s * 1.1 and mean_right_s < guide_right_s * 1.1:
+		#print("Straight")		
+		cv2.putText(image,'Straight',(10,700), cv2.FONT_HERSHEY_COMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
 
 while(True):
 	_, frame = cap.read()
@@ -111,7 +153,7 @@ while(True):
 	processed_image = process_image(colored_image)
 	lines, guide_lines = find_lines(processed_image)
 	draw_lanes(frame,lines)
-	#draw_lanes(frame,guide_lines)
+	#draw_guide_lanes(frame,guide_lines)
 
 	#cv2.imshow('ROI', roi_image)
 	#cv2.imshow('colored_image', colored_image)
